33191a80ed00d66109c498daf5895f0d81819a9e
Kaushal Shukla <35818549+kshukla1bu@users.noreply.github.com>
2022-03-06 15:55:38 -0600

    Added selected Pokemon object

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
+let selectedPokemon = {
+    name: '',
+    baseExperience: null,
+    weight: null,
+    height:  null,
+    type: '',
+    order: null,
+    front_img: '',
+    back_img: '',
+}
+
+
 const fetchPokemon = async (pokemon) => {
     fetch('https://pokeapi.co/api/v2/pokemon/'+ pokemon, {
         method: 'GET',
     }).then(res => {
         res.json().then(r =>{
-            document.getElementById('poke-div-id').innerHTML =
-                "<img class='poke-img-class' id='poke-img' src="+r.sprites.front_default+ " alt='pokemon'/>" +
-                "<p>DESCRIPTION</p>" +
-                "<h3>" + r.forms[0].name.toUpperCase() + "</h3>" +
-                "<p> Base Experience: " + r.base_experience + "</p>" +
-                "<p> Weight: " + r.weight + "</p>" +
-                "<p> Height: "+ r.height +"</p>"
+                selectedPokemon.name  = r.name || '',
+                selectedPokemon.baseExperience  = r.base_experience || null,
+                selectedPokemon.weight = r.weight || null
+                selectedPokemon.height = r.height || null
+                selectedPokemon.type = r.types[0].type.name
+                selectedPokemon.order = r.order || null
+                selectedPokemon.front_img = r.sprites.front_default || ''
+                selectedPokemon.back_img = r.sprites.back_default || ''
         })
     }).catch(e => {
         console.log(e)
     })
 }
+
+console.log('selectedPokemon ->',selectedPokemon)
